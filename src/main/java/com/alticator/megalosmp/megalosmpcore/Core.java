973dee2fc24390ca2244717d9fce13ea6c59aba6
alticator <main@alticator.com>
2022-10-26 11:37:23 +0300

    Add Recipe to Make Logs from Planks
    1. Add a custom recipe to convert eight oak/birch/spruce planks to oak/birch/spruce logs.

--- src/main/java/com/alticator/megalosmp/megalosmpcore/Core.java
+++ src/main/java/com/alticator/megalosmp/megalosmpcore/Core.java
@@ -1,5 +1,9 @@
 package com.alticator.megalosmp.megalosmpcore;
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin {
@@ -16,6 +20,32 @@ public final class Core extends JavaPlugin {
         getServer().getPluginCommand("packprice").setTabCompleter(new PackTabCompleter());
         getServer().getPluginCommand("exchange").setTabCompleter(new ExchangeTabCompleter());
         getServer().getPluginCommand("exchangeprice").setTabCompleter(new ExchangeTabCompleter());
+
+        // Logs to planks recipe
+
+        ItemStack oakLogs = new ItemStack(Material.OAK_LOG, 2);
+        ItemStack spruceLogs = new ItemStack(Material.SPRUCE_LOG, 2);
+        ItemStack birchLogs = new ItemStack(Material.BIRCH_LOG, 2);
+
+        NamespacedKey oakKey = new NamespacedKey(this, "oak_logs");
+        NamespacedKey spruceKey = new NamespacedKey(this, "spruce_logs");
+        NamespacedKey birchKey = new NamespacedKey(this, "birch_logs");
+
+        ShapedRecipe oakRecipe = new ShapedRecipe(oakKey, oakLogs);
+        ShapedRecipe spruceRecipe = new ShapedRecipe(spruceKey, spruceLogs);
+        ShapedRecipe birchRecipe = new ShapedRecipe(birchKey, birchLogs);
+
+        oakRecipe.shape("AAA", "AAA", "A A");
+        spruceRecipe.shape("AAA", "AAA", "A A");
+        birchRecipe.shape("AAA", "AAA", "A A");
+
+        oakRecipe.setIngredient('A', Material.OAK_PLANKS);
+        spruceRecipe.setIngredient('A', Material.SPRUCE_PLANKS);
+        birchRecipe.setIngredient('A', Material.BIRCH_PLANKS);
+
+        getServer().addRecipe(oakRecipe);
+        getServer().addRecipe(spruceRecipe);
+        getServer().addRecipe(birchRecipe);
     }
 
     @Override
